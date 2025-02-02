d2dc2362beeaef15f148931d768930db6731f167
S-Achilleas <sarraxilleas@gmail.com>
2025-02-02 12:23:50 +0200

    Implented the Job class

--- source/src/Job.java
+++ source/src/Job.java
@@ -1,2 +1,17 @@
 public class Job {
+    private final int id;
+    private final int time;
+
+    public Job(int id, int time) {
+        this.id = id;
+        this.time = time;
+    }
+
+    public int getTime() {
+        return time;
+    }
+
+    public int getId() {
+        return id;
+    }
 }
